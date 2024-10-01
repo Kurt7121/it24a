496f60ec7ac54306728d49c3832a226d0ff06cfc
KateC08 <coming.katelyn08@gmail.com>
2024-10-01 11:06:43 +0800

    Added conditional statements

--- app.js
+++ app.js
@@ -19,3 +19,51 @@ let isEqual = (a === b); // Equality check
 
 console.log(`Sum: ${sum}, Difference: ${difference}, Product: ${product}, Quotient: ${quotient}, Is Equal: ${isEqual}`);
 
+// c. Conditional Statements
+
+// i. if else
+if (number % 2 === 0) {
+    console.log(`${number} is even.`);
+} else {
+    console.log(`${number} is odd.`);
+}
+
+// ii. switch
+let day = 3;
+let dayName;
+
+switch (day) {
+    case 1:
+        dayName = "Monday";
+        break;
+    case 2:
+        dayName = "Tuesday";
+        break;
+    case 3:
+        dayName = "Wednesday";
+        break;
+    case 4:
+        dayName = "Thursday";
+        break;
+    case 5:
+        dayName = "Friday";
+        break;
+    case 6:
+        dayName = "Saturday";
+        break;
+    case 7:
+        dayName = "Sunday";
+        break;
+    default:
+        dayName = "Invalid day";
+}
+
+console.log(`The day is: ${dayName}`);
+
+// d. Loops
+
+// For loop to print numbers 1 to 5
+console.log("Numbers from 1 to 5:");
+for (let i = 1; i <= 5; i++) {
+    console.log(i);
+}
